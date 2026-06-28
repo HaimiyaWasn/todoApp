79171637d37c9db2aa5d315714c70f1df472e03d
Wasn <116284029+HaimiyaWasn@users.noreply.github.com>
2026-06-28 11:44:11 +0700

    Update code

---
+++ app/components/TodoApp.tsx
@@ -0,0 +1,33 @@
+"use client"
+
+import Image from "next/image";
+import { useEffect, useState } from "react";
+
+import SideBar from "../layouts/SideBar";
+import BackgroundImage from "@/public/img/backgroundImage/background1.webp";
+
+export default function TodoApp() {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  return (
+    <section className="relative flex min-h-screen overflow-hidden">
+      <Image
+        src={BackgroundImage}
+        alt="Background Image"
+        fill
+        priority
+        className={`object-cover transition-all duration-1500 ease-in-out ${
+          isMounted ? "blur-0 scale-100" : "blur-lg scale-105"
+        }`}
+      />
+
+      <div className="absolute inset-0 bg-black/75" />
+
+      <SideBar/>
+    </section>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-
+import TodoApp from "./components/TodoApp";
 
 export default function Home() {
   return (
     <section>
-      
+      <TodoApp />
     </section>
   );
 }
