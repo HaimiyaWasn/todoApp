b5cf6a3ba4f4de9e20399d894e04ce565848ca8b
Wasn <116284029+HaimiyaWasn@users.noreply.github.com>
2026-07-01 12:59:30 +0700

    Update code

--- app/components/TodoApp.tsx
+++ app/components/TodoApp.tsx
@@ -14,20 +14,28 @@ export default function TodoApp() {
   }, []);
 
   return (
-    <section className="relative flex min-h-screen overflow-hidden">
-      <Image
-        src={BackgroundImage}
-        alt="Background Image"
-        fill
-        priority
-        className={`object-cover transition-all duration-1500 ease-in-out ${
-          isMounted ? "blur-0 scale-100" : "blur-lg scale-105"
-        }`}
-      />
+<section className="relative flex min-h-screen overflow-hidden">
+  <Image
+    src={BackgroundImage}
+    alt="Background"
+    fill
+    priority
+    className="object-cover"
+  />
 
-      <div className="absolute inset-0 bg-black/75" />
+  <div className="absolute inset-0 bg-black/75" />
 
-      <SidebarLayout />
-    </section>
+  <SidebarLayout />
+
+  <main className="relative z-10 flex-1 left-92.5">
+    <header className="h-12 bg-black flex items-center px-6">
+      <h1>Welcome</h1>
+    </header>
+
+    <div className="p-6">
+      Main Content
+    </div>
+  </main>
+</section>
   );
 }
